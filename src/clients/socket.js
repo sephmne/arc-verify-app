e7baf22f1db3de7405a009a98155b6a694e28b14
sephmne <36765535+sephmne@users.noreply.github.com>
2023-12-16 02:29:29 +0000

    correct server

--- src/clients/socket.js
+++ src/clients/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
   process.env.NODE_ENV === "production"
-    ? "viaduct.proxy.rlwy.net:59400"
+    ? "http://viaduct.proxy.rlwy.net:59400"
     : "http://localhost:4000";
 
 export const socket = io(URL);
